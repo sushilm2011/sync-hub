ff48308a6058a7e44f39d170db7a4710ec9900de
Sushil Maurya <sushilm2011@gmail.com>
2024-06-21 21:06:15 +0400

    Initial commit

---
+++ plugins/base_plugin.py
@@ -0,0 +1,33 @@
+class BasePlugin:
+
+  def execute(self, data):
+    raise NotImplementedError("Plugins must implement the execute method")
+
+
+class BaseConnector(BasePlugin):
+
+  def fetch_data(self, callback):
+    raise NotImplementedError(
+        "Connectors must implement the fetch_data method")
+    
+  def execute(self, data):
+    self.fetch_data(data)
+
+
+class BaseTransformer(BasePlugin):
+
+  def transform_data(self, data):
+    raise NotImplementedError(
+        "Transformers must implement the transform_data method")
+    
+  def execute(self, data):
+    self.transform_data(data)
+
+
+class BaseSyncer(BasePlugin):
+
+  def sync_data(self, data):
+    raise NotImplementedError("Syncers must implement the sync_data method")
+  
+  def execute(self, data):
+    self.sync_data(data)
